267af86e381b637212606ad805e7383b16c3ad8d
N-meapp <developersnme@gmail.com>
2026-01-03 20:02:56 +0530

    Django CI/CD initial setup

---
+++ p_app/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomePageTest(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "CI/CD Django App Working")
